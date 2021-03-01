b8606d226b8f144c162a6909a5311ef266c7e5ba
cyborgmg <cyborgmg@gmail.com>
2021-03-01 16:01:47 -0300

    ajuste

--- gateway-service/src/main/java/com/javainuse/exception/ForbiddenException.java
+++
@@ -1,9 +0,0 @@
-package com.javainuse.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class ForbiddenException extends GatewayException {
-    public ForbiddenException(String message) {
-        super(message, HttpStatus.FORBIDDEN);
-    }
-}
--- gateway-service/src/main/java/com/javainuse/exception/UnauthorizedException.java
+++
@@ -1,9 +0,0 @@
-package com.javainuse.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class UnauthorizedException extends GatewayException {
-    public UnauthorizedException(final String message) {
-        super(message, HttpStatus.UNAUTHORIZED);
-    }
-}
